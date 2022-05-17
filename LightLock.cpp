ec710c9a44a4791880832f7f749c47a3c51a2f64
tshibu077 <105731777+tshibu077@users.noreply.github.com>
2022-05-17 18:03:07 -0400

    Update LightLock.cpp

--- LightLock.cpp
+++ LightLock.cpp
@@ -2,6 +2,8 @@
 #include "LightLock.h"
 #include "MyRGB.h"
 
+#define DELAY_SPDT 50
+
 LightLock::LightLock (int sw1, int sw2, int sw3, int sw4, int r1, int g1, int b1, int r2, int g2, int b2, int r3, int g3, int b3): led1(r1,g1,b1,sw1), led2(r1,g2,b2,sw2), led3(r1,g3,b3,sw3) 
 {
   _sw4 = sw4;
@@ -96,5 +98,7 @@ void LightLock::updateLightLock()
     nextColor();
     _swStateLast4 = _swState4;
   }
+  
+  delay(DELAY_SPDT);
 
 }
