aec80730ae975bd2e5996b2eafe8653510a543a1
tshibu077 <105731777+tshibu077@users.noreply.github.com>
2022-05-17 17:56:56 -0400

    Update MyRGB.cpp

--- MyRGB.cpp
+++ MyRGB.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include "MyRGB.h"
 
-/*Constructor*/
+#define blinkRate 150
+
+/*Constructor without switch*/
 MyRGB::MyRGB(int redPin, int greenPin, int bluePin) {
   pinMode(redPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
@@ -12,16 +14,36 @@ MyRGB::MyRGB(int redPin, int greenPin, int bluePin) {
   _bluePin = bluePin;
 }
 
+/*Constructor with switch*/
+MyRGB::MyRGB(int redPin, int greenPin, int bluePin, int switchPin) {
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+  
+  _redPin = redPin;
+  _greenPin = greenPin;
+  _bluePin = bluePin;
+  sw = switchPin;
+}
+
+/*Functions to make setting a color simple in main program*/
+void MyRGB::redRGB() {
+  analogWrite(_redPin, 255);
+  analogWrite(_greenPin, 0);
+  analogWrite(_bluePin, 0);
+}
+
 void MyRGB::greenRGB() {
   analogWrite(_redPin, 0);
   analogWrite(_greenPin, 255);
   analogWrite(_bluePin, 0);
 }
 
-void MyRGB::redRGB() {
-  analogWrite(_redPin, 255);
+void MyRGB::blueRGB() {
+  analogWrite(_redPin, 0);
   analogWrite(_greenPin, 0);
-  analogWrite(_bluePin, 0);
+  analogWrite(_bluePin, 255);
 }
 
 void MyRGB::whiteRGB() {
@@ -36,13 +58,17 @@ void MyRGB::offRGB() {
   analogWrite(_bluePin, 0);
 }
 
-void MyRGB::blinkRGB(int blinkRate) { 
-  digitalWrite(_bluePin, HIGH);
-  digitalWrite(_redPin, HIGH);
-  digitalWrite(_greenPin, HIGH);
-  delay(blinkRate); 
-  digitalWrite(_bluePin, LOW);
-  digitalWrite(_redPin, LOW);
-  digitalWrite(_greenPin, LOW);  
-  delay(blinkRate);
+/*Function that blinks the LED three times*/
+void MyRGB::blinkThrice() { 
+  for (int i=0; i<3; i++) 
+  {
+    digitalWrite(_bluePin, HIGH);
+    digitalWrite(_redPin, HIGH);
+    digitalWrite(_greenPin, HIGH);
+    delay(blinkRate); 
+    digitalWrite(_bluePin, LOW);
+    digitalWrite(_redPin, LOW);
+    digitalWrite(_greenPin, LOW);  
+    delay(blinkRate);
+  }
 }
