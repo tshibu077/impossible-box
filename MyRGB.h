34b2b2b7f4c111e331f83a244058781093fab655
tshibu077 <105731777+tshibu077@users.noreply.github.com>
2022-06-04 15:52:00 -0400

    Update MyRGB.h

--- MyRGB.h
+++ MyRGB.h
@@ -1,7 +1,9 @@
 #ifndef MyRGB_H
 #define MyRGB_H
 
-#include <Arduino.h>
+#include "Arduino.h"
+
+//RGB LED digital output pins
 
 class MyRGB {
   public: 
@@ -12,15 +14,19 @@ class MyRGB {
     void redRGB();
     void blueRGB();
     void whiteRGB();
-    void blinkThrice(int blinkRate);
+    void blinkThrice();
     void offRGB();
+    int updateSwitch();
+    void updateLed(String color, int on);
 
     int sw;
-
+    
   private:
     int _redPin;
     int _bluePin;
     int _greenPin;   
+    int swState;
+    int swStateLast;
 
 };
 
